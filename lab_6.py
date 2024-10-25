90f6dac77ae7d08fd3c07e65f1fd7b3b95373e32
Josh <Josh@4A-6F-73-68.local>
2024-10-25 17:05:36 -0400

    Final Update

--- lab_6.py
+++ lab_6.py
@@ -1,28 +1,28 @@
 def encode(password):
     encoded = ''
     for num in password:
-        encoded += str(int(num) + 3)
+        encoded += str(int(num) + 3)[-1]
     return encoded
+
 
 if __name__ == '__main__':
 
     while True:
 
-        #changed
-        
+        #changed again
+
         print('\nMenu\n'
               '-------------\n'
               '1. Encode\n'
               '2. Decode\n'
               '3. Quit\n')
 
-        selection = input('\nPlease enter an option: ')
+        selection = input('Please enter an option: ')
 
         if selection == '1':
-            password = input('Please enter your password to encode: ')
-            encoded = encode(password)
-            print(encoded)
+            encoded = encode(input('Please enter your password to encode: '))
+            print('Your password has been encoded and stored!')
         elif selection == '2':
-            exit()
+            print(f'The encoded password is {encoded} and the original password is {''}')
         elif selection == '3':
             exit()
